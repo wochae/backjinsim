f84ca3a70ae19626ec4022e6cf74ad3e4d44c6e4
wochae <officialwochae@gmail.com>
2024-08-28 22:27:05 +0900

    [feat/#3] : Letter Persist

--- src/main/java/com/jinsim/back/letters/LetterRepository.java
+++ src/main/java/com/jinsim/back/letters/LetterRepository.java
@@ -1,8 +1,12 @@
 package com.jinsim.back.letters;
 
 import com.jinsim.back.letters.entity.Letter;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LetterRepository extends JpaRepository<Letter, Long> {
 
+    List<Letter> findByRecipientId(Long userId);
+
+    List<Letter> findByAuthorId(Long userId);
 }
